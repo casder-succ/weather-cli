f4d7f621d5151a4163263057f3f3386c15153876
casder <casderiopus@gmail.com>
2022-11-25 01:24:45 +0300

    Add logger service

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/index.js';
+import { loggerService } from './services/index.js';
 
 const initCLI = () => {
   const args = getArgs(process.argv);
 
-  console.log(args);
+  if (args.h) {
+    loggerService.printHelp();
+  }
 };
 
 initCLI();
